86559f87eedab3e66db98abbd70124e473d73f3a
Rizki Syaban Aryanto <rizkisyaban2@gmail.com>
2020-01-02 14:39:52 +0700

    Create function newDeck() in deck.go and call it in main.go

--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
-func newCard() string {
-	return "Five of Diamonds"
-}
-
 func main() {
 
-	cards := deck{newCard(), "One of Diamonds"}
-	cards = append(cards, "Two of Heart")
+	cards := newDeck()
 
 	cards.print()
 
